48d0a4878932decd25fd8a54a4b2a28b48fe738b
Tobias Lundin <tobias.lundin@gmail.com>
2022-01-22 22:03:57 +0100

    sort packagejson add react jsx 17 runtime

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactJsx from 'vite-react-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), reactJsx()],
   server: {
     port: 40800
   },
   optimizeDeps: {
     include: ['shaka-player']
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`
   }
 })
